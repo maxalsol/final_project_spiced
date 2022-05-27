ea8cc9a0087165445955bc9c9a89b2e4ef22ed77
maxalsol <max.pallat@gmail.com>
2022-05-27 13:52:04 +0200

    start Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1>Header</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
