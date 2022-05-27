e141873f31f0be1c1c0c131b1b73c3cb3db2a821
maxalsol <max.pallat@gmail.com>
2022-05-27 13:36:08 +0200

    styled Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,72 @@
 // rfce to use snippet to create functional component
 
 import React from "react";
+import myLogo from "../logo.png";
+//
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">
-                    Navbar
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+            <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+                <div className="container">
+                    <a className="navbar-brand" href="#">
+                        <img className="my-logo" src={myLogo} alt="logo"></img>
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <FontAwesomeIcon
+                            icon={faBars}
+                            className="nav-hamburger"
+                        />
+                    </button>
 
-                <div
-                    className="collapse navbar-collapse"
-                    id="navbarSupportedContent"
-                >
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">
-                                Home <span className="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">
-                                Link
-                            </a>
-                        </li>
-                    </ul>
+                    <div
+                        className="collapse navbar-collapse"
+                        id="navbarSupportedContent"
+                    >
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">
+                                    Home{" "}
+                                    <span className="sr-only">(current)</span>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
+                                    About
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
+                                    Services
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
+                                    Experience
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
+                                    Portfolio
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </>
