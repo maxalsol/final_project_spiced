cb6c2e0c8c9090acaa47f14aeb3614699ed7e6b9
maxalsol <max.pallat@gmail.com>
2022-05-28 10:58:03 +0200

    Services component start

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Particles from "react-tsparticles";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
+import Services from "./components/Services";
 
 function App() {
     return (
@@ -91,6 +92,7 @@ function App() {
             <Navbar />
             <Header />
             <About />
+            <Services />
         </>
     );
 }
---
+++ src/components/Services.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Services() {
+    return (
+        <div className="servies">
+            <h1 className="py-5">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-3 col-sm-6">
+                            <div className="box">
+                                <h3>Web Development</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </h1>
+        </div>
+    );
+}
+
+export default Services;
