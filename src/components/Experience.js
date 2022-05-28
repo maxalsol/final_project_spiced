3e1e2f6dbd674490bebd2ccfce33c96be4316293
maxalsol <max.pallat@gmail.com>
2022-05-28 18:55:41 +0200

    Experience component structure and content

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,6 +6,78 @@ function Experience() {
             <div className="d-flex justify-content-center my-5">
                 <h1>Experience</h1>
             </div>
+            <div className="container experience-wrapper">
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-contene">
+                        <h3>2017-2018</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Vivamus suscipit tortor eget felis porttitor
+                            volutpat. Donec rutrum congue leo eget malesuada.
+                            Vestibulum ac diam sit amet quam vehicula elementum
+                            sed sit amet dui. Quisque velit nisi, pretium ut
+                            lacinia in, elementum id enim.
+                        </p>
+                    </div>
+                </div>
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-contene">
+                        <h3>2018-2020</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Vivamus suscipit tortor eget felis porttitor
+                            volutpat. Donec rutrum congue leo eget malesuada.
+                            Vestibulum ac diam sit amet quam vehicula elementum
+                            sed sit amet dui. Quisque velit nisi, pretium ut
+                            lacinia in, elementum id enim.
+                        </p>
+                    </div>
+                </div>
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-contene">
+                        <h3>2020-2021</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Vivamus suscipit tortor eget felis porttitor
+                            volutpat. Donec rutrum congue leo eget malesuada.
+                            Vestibulum ac diam sit amet quam vehicula elementum
+                            sed sit amet dui. Quisque velit nisi, pretium ut
+                            lacinia in, elementum id enim.
+                        </p>
+                    </div>
+                </div>
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-contene">
+                        <h3>2021-2022</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Vivamus suscipit tortor eget felis porttitor
+                            volutpat. Donec rutrum congue leo eget malesuada.
+                            Vestibulum ac diam sit amet quam vehicula elementum
+                            sed sit amet dui. Quisque velit nisi, pretium ut
+                            lacinia in, elementum id enim.
+                        </p>
+                    </div>
+                </div>
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-contene">
+                        <h3>2022-present</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Vivamus suscipit tortor eget felis porttitor
+                            volutpat. Donec rutrum congue leo eget malesuada.
+                            Vestibulum ac diam sit amet quam vehicula elementum
+                            sed sit amet dui. Quisque velit nisi, pretium ut
+                            lacinia in, elementum id enim.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
