ac99192ec4d5b08090b23c28d1c9e275db3d72ae
maxalsol <max.pallat@gmail.com>
2022-05-30 16:57:57 +0200

    Contact component start

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from "./components/About";
 import Services from "./components/Services";
 import Experience from "./components/Experience";
 import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
 
 function App() {
     return (
@@ -97,6 +98,7 @@ function App() {
             <Services />
             <Experience />
             <Portfolio />
+            <Contact />
         </>
     );
 }
