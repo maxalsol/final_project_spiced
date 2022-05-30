9f1b5ebdf9ff8b83eabd96e986e195f1a47ac846
maxalsol <max.pallat@gmail.com>
2022-05-30 14:50:50 +0200

    Portfolio component icons and transition styled

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,35 +3,64 @@ import netflix from "../images/netflix.png";
 import cityGuide from "../images/city-guide-app.png";
 import portfolio from "../images/portfolio.png";
 import taskManager from "../images/task-manager.png";
+// FONT AWESOME  ICONS
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
 
 function Portfolio() {
     return (
         <div className="portfolio-wrapper">
             <div className="container">
                 <h1 className="text-uppercase text-center py-5">Portfolio</h1>
-                <div className="image-box-wrapper row justify-content-center">
-                    <img className="portfolio-image" src={netflix} alt=""></img>
-                </div>
-                <div className="image-box-wrapper row justify-content-center">
-                    <img
-                        className="portfolio-image"
-                        src={cityGuide}
-                        alt=""
-                    ></img>
-                </div>
-                <div className="image-box-wrapper row justify-content-center">
-                    <img
-                        className="portfolio-image"
-                        src={portfolio}
-                        alt=""
-                    ></img>
-                </div>
-                <div className="image-box-wrapper row justify-content-center">
-                    <img
-                        className="portfolio-image"
-                        src={taskManager}
-                        alt=""
-                    ></img>
+                <div className="image-box-wrapper row row-cols-auto justify-content-center">
+                    <div className="portfolio-image-box">
+                        <img
+                            className="portfolio-image"
+                            src={netflix}
+                            alt=""
+                        ></img>
+                        <div className="overflow"></div>
+                        <FontAwesomeIcon
+                            className="portfolio-icon"
+                            icon={faSearchPlus}
+                        />
+                    </div>
+                    <div className="portfolio-image-box">
+                        <img
+                            className="portfolio-image"
+                            src={cityGuide}
+                            alt=""
+                        ></img>
+                        <div className="overflow"></div>
+                        <FontAwesomeIcon
+                            className="portfolio-icon"
+                            icon={faSearchPlus}
+                        />
+                    </div>
+                    <div className="portfolio-image-box">
+                        <img
+                            className="portfolio-image"
+                            src={portfolio}
+                            alt=""
+                        ></img>
+                        <div className="overflow"></div>
+                        <FontAwesomeIcon
+                            className="portfolio-icon"
+                            icon={faSearchPlus}
+                        />
+                    </div>
+                    <div className="portfolio-image-box">
+                        <img
+                            className="portfolio-image"
+                            src={taskManager}
+                            alt=""
+                        ></img>
+                        <div className="overflow"></div>
+                        <FontAwesomeIcon
+                            className="portfolio-icon"
+                            icon={faSearchPlus}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
