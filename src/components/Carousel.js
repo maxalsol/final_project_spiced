415a7364a95e91b106ea85f5ee8826f192c879db
maxalsol <max.pallat@gmail.com>
2022-06-02 11:10:02 +0200

    production push error

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,7 +5,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import avatar1 from "../images/avatars/avatar-1.png";
 import avatar2 from "../images/avatars/avatar-2.png";
 import avatar3 from "../images/avatars/avatar-3.png";
-import avatar4 from "../images/avatars/avatar-4.png";
+// import avatar4 from "../images/avatars/avatar-4.png";
 
 const TestimonialsCarousel = () => {
     return (
